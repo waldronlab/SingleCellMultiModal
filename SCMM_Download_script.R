52b08d01d2f714104ff8ed628250b45230376454
kbeckenrode <kbeckenrode@gmail.com>
2019-11-19 11:24:58 -0500

    added getGEO function

--- SCMM_Download_script.R
+++ SCMM_Download_script.R
@@ -1,2 +1,9 @@
 print ("I made a script file to beging version control")
 print ("hi world")
+library(GEOquery)
+browseVignettes("GEOquery")
+?getGEO
+#comment here cuz I added a hashtag
+# gsm <- getGeo('GSM2695379') 
+gsm <- getGEO('GSM2695379')
+class(gsm)
