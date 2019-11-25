22b0c239e095cc3c3be7ba8d35b16f9e417c41c8
kbeckenrode <kbeckenrode@gmail.com>
2019-11-25 18:20:41 -0500

    Edits

--- SCMM_Download_script.R
+++ SCMM_Download_script.R
@@ -1,11 +1,8 @@
-print ("I made a script file to beging version control")
-print ("hi world")
+#	CITE-seq: Large scale simultaneous measuremnt of epitopes and transcriptomes in single cells
 library(GEOquery)
 browseVignettes("GEOquery")
 ?getGEO
-#comment here cuz I added a hashtag
 # gsm <- getGeo('GSM2695379') 
 gsm <- getGEO('GSM2695379')
 class(gsm)
-install.package("Rmarkdown")
-install.packages ("rmarkdown")
+gsm <- getGEO('GSM2695380')
