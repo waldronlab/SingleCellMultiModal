b34076878c2c3f08b6033acad5455aac57b28d08
kbeckenrode <kbeckenrode@gmail.com>
2019-11-26 12:52:16 -0500

    Added ExpressionSet script

--- SCMM_Download_script.R
+++ SCMM_Download_script.R
@@ -1,8 +1,20 @@
 #	CITE-seq: Large scale simultaneous measuremnt of epitopes and transcriptomes in single cells
 library(GEOquery)
-browseVignettes("GEOquery")
-?getGEO
 # gsm <- getGeo('GSM2695379') 
 gsm <- getGEO('GSM2695379')
-class(gsm)
-gsm <- getGEO('GSM2695380')
+#ADT: CBMC_8K_13AB_10x info for above script
+##
+library(Biobase)
+example(ExpressionSet)
+
+eset <- expresssionSet
+
+write.table(exprs(eset), file = "test.txt")
+## 
+## read data
+
+myeset <- read.table("test.txt")
+
+library(SummarizedExperiment)
+SummarizedExperiment(assays = list(counts = data.matrix(myeset)))
+
