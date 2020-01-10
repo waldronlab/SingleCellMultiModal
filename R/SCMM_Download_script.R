13b56127ac3d82f59cf5a1975e0a6e954897890f
LiNk-NY <marcel.ramosperez@roswellpark.org>
2020-01-10 10:55:03 -0500

    minor updates

--- R/SCMM_Download_script.R
+++ R/SCMM_Download_script.R
@@ -7,7 +7,13 @@ gse <- getGEO('GSE121708')
 # )
 
 # download raw data
-# dfdata <- getGEOSuppFiles("GSE121708")
+dfdata <- getGEOSuppFiles("GSE121708")
+files <- untar("GSE121708/GSE121708_RAW.tar", list = TRUE)
+
+# obtain just file location
+testdata <- getGEOSuppFiles("GSE121708", fetch_files = FALSE)
+
+sampids <- vapply(strsplit(files, "_"), `[[`, character(1L), 1L)
 
 # identical(
 #     pData(gse[[1]])$geo_accession,
@@ -15,7 +21,24 @@ gse <- getGEO('GSE121708')
 # )
 # TRUE
 
-gg <- head(pData(gse[[1]])$geo_accession)
+geos <- pData(gse[[1]])$geo_accession
+sum(geos %in% sampids)/length(geos) * 100
+
+ago <- lapply(head(geos), function(acc) {
+    Meta(getGEO(acc))$relation
+})
+
+notes <- notes(experimentData(gse[[1]]))$relation
+sups <- gsub(".*(GSE*)", "\\1",
+grep("SuperSeries", unlist(strsplit(notes, "\n")), ignore.case = TRUE, value = TRUE)
+)
+lapply(sups, getGEO)
+
+
+library(SRAdb)
+sra_con <- dbConnect(SQLite(), getSRAdbFile())
+listSRAfile("SRX4917371", sra_con)
+
 metlist <- lapply(gg, getGEO)
 metas <- lapply(metlist, Meta)
 names(metas) <- gg
