7d36c505a8457aa99f94740de9b96f2e5015b579
LiNk-NY <marcel.ramosperez@roswellpark.org>
2020-01-23 18:04:25 -0500

    add more exploratory code

--- R/SCMM_Download_script.R
+++ R/SCMM_Download_script.R
@@ -1,6 +1,7 @@
 # scNMT
 library(GEOquery)
-gse <- getGEO('GSE121708')
+gse <- getGEO('GSE121708', GSEMatrix = TRUE)
+# gse <- getGEO('GSE121708', GSEMatrix = FALSE)
 
 # head(
 #     pData(phenoData(gse[[1]]))
@@ -9,6 +10,11 @@ gse <- getGEO('GSE121708')
 # download raw data
 dfdata <- getGEOSuppFiles("GSE121708")
 files <- untar("GSE121708/GSE121708_RAW.tar", list = TRUE)
+test <- head(files)
+untar("GSE121708/GSE121708_RAW.tar", files = test)
+gsm0 <- read.table("GSM3443369_E4.5-5.5_new_Plate1_A02_met.tsv.gz", header = TRUE)
+gsm1 <- readr::read_tsv("GSM3443369_E4.5-5.5_new_Plate1_A02_met.tsv.gz")
+gsm1.5 <- readr::read_tsv("GSM3443369_E4.5-5.5_new_Plate1_A02_acc.tsv.gz")
 
 # obtain just file location
 testdata <- getGEOSuppFiles("GSE121708", fetch_files = FALSE)
@@ -32,12 +38,23 @@ notes <- notes(experimentData(gse[[1]]))$relation
 sups <- gsub(".*(GSE*)", "\\1",
 grep("SuperSeries", unlist(strsplit(notes, "\n")), ignore.case = TRUE, value = TRUE)
 )
-lapply(sups, getGEO)
-
+slups <- lapply(sups, getGEO, GSEMatrix = FALSE)
+lapply(slups, function(x) lapply(GSMList(x), function(y) Meta(y)$title))
 
 library(SRAdb)
-sra_con <- dbConnect(SQLite(), getSRAdbFile())
-listSRAfile("SRX4917371", sra_con)
+# sra_con <- dbConnect(SQLite(), getSRAdbFile())
+sra_con <- dbConnect(SQLite(), "SRAmetadb.sqlite")
+listSRAfile("SRX4917371", sra_con, fileType = "sra")
+getSRAinfo("SRX4917371", sra_con, sraType = "sra" )
+getSRAfile("SRX4917371", sra_con, fileType = "sra" )
+listSRAfile ("SRX4917371", sra_con, fileType = 'sra', srcType='fasp')
+
+getSRAinfo("SRS3964271", sra_con, sraType = "sra")
+getSRAinfo("SRR8091082", sra_con)
+sraConvert("SRR8091082", out_type = c("study", "sample", "experiment", "run"), sra_con)
+
+getGEO("GSM3440983", GSEMatrix = FALSE)
+# https://trace.ncbi.nlm.nih.gov/Traces/sra/?run=SRR8091082
 
 metlist <- lapply(gg, getGEO)
 metas <- lapply(metlist, Meta)
@@ -50,3 +67,14 @@ lapply(metas, function(x) as(x, "CharacterList"))
 
 
 getGSEDataTables('GSE121708')
+
+library(dbplyr)
+library(dplyr)
+
+src_dbi(sra_con)
+
+tbl(sra_con, "run")
+
+res <- httr::POST("https://www.ncbi.nlm.nih.gov/Traces/sdl/2/retrieve?acc=SRR8091082,SRR8091083")
+reslist <- httr::content(res)
+
