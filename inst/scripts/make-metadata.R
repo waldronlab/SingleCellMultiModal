e69f9b0325560cf28c053c04e8927d8e1d0e0e6b
LiNk-NY <mramos09@gmail.com>
2020-09-14 11:29:15 -0400

    add documentation and update make_docu.R

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -28,10 +28,65 @@ allextpat <- "\\.[Rr][Dd][Aa]$"
 setwd("~/gh/SingleCellMultiModal")
 source("inst/extdata/docuData/scNMT.R")
 
+#' Generate the metadata.csv file from a documentation file
+#'
+#' This function takes a specific folder structure and generates the
+#' metadata.csv file for adding to ExperimentHub.
+#'
+#' @param directory The base folder for _all_ datasets
+#'
+#' @param dataDirs character() A vector of folder names contained in directory
+#'     that corresponds to each project. For multiple versions, repeat the
+#'     name of the folder.
+#'
+#' @param version character() A vector of subfolder versions that is parallel
+#'     to `dataDirs` argument, typically `v1.0.0`.
+#'
+#' @param ext_pattern character(1) A string that matches files within the
+#'     above folders to find the data.
+#'
+#' @param doc_file character(1) A path to the documentation `data.frame` that
+#'     tells the function how to fill in the standard columns for data
+#'     annotation, for example `DataProvider`, `TaxonomyId`, etc.
+#'
+#' @param pkg_name character(1) The name of the current package
+#'
+#' @param dry.run logical(1) Whether to (over)write the `metadata.csv` file or
+#'     return as output.
+#'
+#' @param append logical(1) Whether to append to the current `metadata.csv`
+#'     file
+#'
+#' @return Saves a file under `/inst/extdata/metadata.csv`
+#'
+#' @examples
+#'
+#' make_metadata(
+#'     directory = "~/data/scmm"
+#'     dataDirs = "mouse_gastrulation",
+#'     version = c("1.0.0", "2.0.0"),
+#'     doc_file = "inst/extdata/docuData/singlecellmultimodalv2.csv",
+#'     dry.run = FALSE
+#' )
+#'
+#' make_metadata(
+#'     directory = "~/data/scmm",
+#'     dataDirs = c(rep("mouse_gastrulation", 2),
+#'         rep("mouse_visual_cortex", 2)),
+#'     version = rep(c("1.0.0", "2.0.0"), 2),
+#'     ext_pattern = "\\.[Rr][Dd][Aa]$",
+#'     doc_file = "inst/extdata/docuData/singlecellmultimodalv3.csv",
+#'     pkg_name = "SingleCellMultiModal",
+#'     dry.run = TRUE,
+#' )
+#'
+#' @md
+#'
+#' @export
 make_metadata <- function(
     directory = "~/data/scmm",
-    dataDirs = c(rep("mouse_gastrulation", 2), "mouse_visual_cortex"),
-    version = c("1.0.0", "2.0.0"),
+    dataDirs = c(rep("mouse_gastrulation", 2), rep("mouse_visual_cortex", 2)),
+    version = rep(c("1.0.0", "2.0.0"), 2),
     ext_pattern = "\\.[Rr][Dd][Aa]$",
     doc_file,
     pkg_name = "SingleCellMultiModal",
--- inst/scripts/make_docu.R
+++ inst/scripts/make_docu.R
@@ -35,23 +35,24 @@ write.csv(
     row.names = FALSE
 )
 
-# version 2 with spatial
+# version 3 with spatial
 scmeta <- data.frame(
     DataProvider = c(
         rep("Dept. of Bioinformatics, The Babraham Institute, United Kingdom", 2),
-        "Dept. of Molecular Genetics, Allen Institute for Brain Science, United States"
+        rep("Dept. of Molecular Genetics, Allen Institute for Brain Science, United States", 2)
     ),
     TaxonomyId = "10090",
     Species = "Mus musculus",
     SourceUrl = c(
         rep("https://cloudstor.aarnet.edu.au/plus/s/Xzf5vCgAEUVgbfQ", 2),
+        "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE71585",
         "https://www.dropbox.com/sh/avj4nrd4la5i88u/AACafWwBbE-xsLvOGDwRZDpYa?dl=0"
-     ),
-    SourceType = "RDS",
-    SourceVersion = c("1.0.0", "2.0.0", "1.0.0"),
-    DataType = c(rep("mouse_gastrulation", 2), "mouse_visual_cortex"),
+    ),
+    SourceType = c("RDS", "RDS", "TXT", "TXT"),
+    SourceVersion = c("1.0.0", "2.0.0", "1.0.0", "2.0.0"),
+    DataType = c(rep("mouse_gastrulation", 2), rep("mouse_visual_cortex", 2)),
     Maintainer = c(rep("Marcel Ramos <marcel.ramos@roswellpark.org>", 2),
-        "Dario Righelli <dario.righelli@gmail.com>"),
+        rep("Dario Righelli <dario.righelli@gmail.com>", 2)),
     stringsAsFactors = FALSE
 )
 write.csv(
