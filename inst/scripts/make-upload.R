90f5ccd7f18212da821b467852f662eb81e83726
LiNk-NY <mramos09@gmail.com>
2020-12-22 14:56:55 -0500

    update upload script

--- inst/scripts/make-upload.R
+++ inst/scripts/make-upload.R
@@ -21,7 +21,7 @@ allextpat <- "\\.[Rr][Dd][AaSs]$|\\.[Mm][Tt][Xx]\\.[Gg][Zz]$|\\.[Hh]5$"
 # Make sure that AWS_DEFAULT_REGION, AWS_ACCESS_KEY_ID, and
 # AWS_SECRET_ACCESS_KEY are set in the ~/.Renviron file
 
-source("inst/scripts/make-metadata.R")
+# source("inst/scripts/make-metadata.R")
 
 upload_aws <- function(
     DataType, directory = "~/data/scmm", upload = FALSE,
@@ -36,13 +36,20 @@ upload_aws <- function(
     vfolder <- .version_folder(version)
     bucketLocation <-
         file.path("experimenthub", "SingleCellMultiModal", DataType, vfolder)
+    if (!upload)
+        message("Data NOT uploaded")
     if (upload)
         AnnotationHubData:::upload_to_S3(file = datafilepaths,
             remotename = basename(datafilepaths),
             bucket = bucketLocation)
+    else
+        file.path("s3:/", bucketLocation, basename(datafilepaths))
 }
 
 # upload_aws(DataType = "mouse_gastrulation", version = "1.0.0", upload=TRUE)
 # upload_aws(DataType = "mouse_gastrulation", version = "2.0.0", upload=TRUE)
-# upload_aws(DataType = "cord_blood", directory="CITEseq", version = "1.0.0", upload=TRUE)
+# upload_aws(DataType = "cord_blood", directory="CITEseq", version = "1.0.0",
+#     upload=TRUE)
 # upload_aws(DataType = "mouse_visual_cortex", upload=TRUE)
+# upload_aws(DataType = "pbmc_10x", directory = "~/data/scmm",
+#     version = "1.0.0", upload = TRUE)
