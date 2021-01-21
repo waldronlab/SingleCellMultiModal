6c8bdddee2af0a97b666c46c9ec3603ff674bbac
LiNk-NY <mramos09@gmail.com>
2021-01-21 11:04:11 -0500

    update make-data.R file

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,31 +1,15 @@
-# convert .csv files to .rda matrices
-.convertData <- function(
-    directory = "~/data/scmm/",
-    dataDir = "mouse_gastrulation",
-    pattern = ".csv")
-{
-    location <- file.path(directory, dataDir)
-    csvs <- list.files(location, pattern = pattern, full.names = TRUE,
-        recursive = FALSE)
-    invisible(
-        lapply(csvs, function(csvfile) {
-            objname <- gsub(pattern, "", basename(csvfile))
-            readin <- as.data.frame(readr::read_csv(csvfile))
-            rnames <- readin[[1L]]
-
-            if (!objname %in% c("scnmt_colData", "scnmt_sampleMap"))
-                readin <- data.matrix(readin[, -1])
-            else if (identical(objname, "scnmt_colData"))
-                names(readin)[1] <- "cellID"
-            else
-                readin <- readin[, -1]
-
-            if (!objname %in% "scnmt_sampleMap")
-                rownames(readin) <- rnames
-
-            assign(objname, readin)
-            rdafile <- gsub("csv", "rda", csvfile)
-            save(list = objname, file = rdafile)
-        })
-    )
-}
+##
+## PLACEHOLDER for make-data/
+##
+## see the respective R script for each technology
+##
+## for example, make-data/scNMT.R for an example on how
+## we took a MultiAssayExperiment object and created
+## Rda files for upload to ExperimentHub
+##
+## see make-data/scMultiome.R for converting a MultiAssayExperiment
+## into sparceMatrix representations using HDF5 and MTX formats
+##
+## see make-data/make-macrophage.R for taking raw data and
+## creating SingleCellExperiment data products
+##
