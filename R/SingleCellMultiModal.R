ed8423b5cee42242fc9765321ef86dc1510c12ad
LiNk-NY <mramos09@gmail.com>
2021-03-15 14:04:50 -0400

    update pan function

--- R/SingleCellMultiModal.R
+++ R/SingleCellMultiModal.R
@@ -38,11 +38,8 @@
 #' @examples
 #'
 #' SingleCellMultiModal(c("mouse_gastrulation", "pbmc_10x"),
-#'     modes = list(c("acc*", "met*"), "*"),
+#'     modes = list(c("acc*", "met*"), "rna"),
 #'     version = c("2.0.0", "1.0.0"), dry.run = TRUE, verbose = TRUE
-#' )
-#' SingleCellMultiModal(c("mouse_gastrulation", "pbmc_10x"), modes = "*",
-#'     version = c("2.0.0", "1.0.0"), dry.run = FALSE, verbose = TRUE
 #' )
 #'
 #' @export
@@ -69,29 +66,12 @@ SingleCellMultiModal <- function(
 
     if (dry.run) { return(ess_lists) }
 
-    ## TODO: work with ess_lists to conver to MultiAssayExperiment
-    new_prefix <- paste0(resmap[["datatype"]], "_")
-    exps <- Map(function(x, y) {
-        exs <- x[["experiments"]]
-        names(exs) <- paste0(y, names(exs))
-        exs
+    new_prefix <- paste0(resmap[["DataType"]], "_")
+    ess_lists <- Map(function(x, y) {
+        if (is(x, "MultiAssayExperiment"))
+            names(x) <- paste0(y, names(x))
+        x
     }, x = ess_lists, y = new_prefix)
-    exps <- do.call(c, unname(exps))
 
-    samps <- Map(function(x, y) {
-        samps <- x[["sampleMap"]]
-        samps[["assay"]] <- paste0(y, samps[["assay"]])
-        as(samps, "DataFrame")
-    }, x = ess_lists, y = new_prefix)
-    samps <- do.call(BiocGenerics::rbind, unname(samps))
-
-    coldata <- Reduce(function(x, y) {
-        merge(x, y, by = intersect(names(x), names(y)), all = TRUE)
-    }, lapply(ess_lists, `[[`, "colData"))
-
-    MultiAssayExperiment(
-        experiments = exps,
-        colData = coldata,
-        sampleMap = samps
-    )
+    Reduce(c, ess_lists)
 }
