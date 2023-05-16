d0cd6ee39a9af30f84d8117a606ae5ac9276c609
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-05-16 14:29:29 -0400

    whitespace and formatting

--- R/CITEseq.R
+++ R/CITEseq.R
@@ -11,7 +11,7 @@
     return(mae)
 }
 
-.combMatrixForAssay <- function(explist, dimslist, 
+.combMatrixForAssay <- function(explist, dimslist,
                                 assayId=c("scADT", "scHTO", "scRNA"))
 {
     match.arg(assayId)
@@ -20,9 +20,9 @@
            "scADT"=, "scHTO"={
                if(length(explist[assIdx]) == 2)
                {
-                   m1 <- Matrix::Matrix(unlist(explist[assIdx]), 
+                   m1 <- Matrix::Matrix(unlist(explist[assIdx]),
                         nrow=dimslist[assIdx][[1]][1],
-                        ncol=(dimslist[assIdx][[1]][2]+dimslist[assIdx][[2]][2]), 
+                        ncol=(dimslist[assIdx][[1]][2]+dimslist[assIdx][[2]][2]),
                         sparse=TRUE)
                } else {
                    m1 <- Matrix::Matrix(explist[[assIdx]])
@@ -41,19 +41,19 @@
     )
     if(length(explist[assIdx]) == 2)
     {
-        colnames(m1) <- c(paste0(rep(gsub("scADT|scHTO|scRNA","", 
+        colnames(m1) <- c(paste0(rep(gsub("scADT|scHTO|scRNA","",
                                           names(explist)[assIdx[1]]),
-                                     dimslist[assIdx][[1]][2]), 
+                                     dimslist[assIdx][[1]][2]),
                                  colnames(explist[[assIdx[1]]])),
-                          paste0(rep(gsub("scADT|scHTO|scRNA","", 
+                          paste0(rep(gsub("scADT|scHTO|scRNA","",
                                           names(explist)[assIdx[2]]),
-                                     dimslist[assIdx][[2]][2]), 
+                                     dimslist[assIdx][[2]][2]),
                                  colnames(explist[[assIdx[2]]])))
         rownames(m1) <- rownames(explist[[assIdx[[1]]]])
     } else {
-        colnames(m1) <- paste0(rep(gsub("scADT|scHTO|scRNA","", 
+        colnames(m1) <- paste0(rep(gsub("scADT|scHTO|scRNA","",
                                         names(explist)[assIdx[1]]),
-                                   dimslist[assIdx][[1]][2]), 
+                                   dimslist[assIdx][[1]][2]),
                                colnames(explist[[assIdx[1]]]))
         rownames(m1) <- rownames(explist[[assIdx[[1]]]])
     }
@@ -72,9 +72,9 @@
 .buildMap <- function(mat1, assayId)
 {
     map <- DataFrame(assay=assayId,
-                     #primary=gsub("_\\w+", "", colnames(mat1)), 
+                     #primary=gsub("_\\w+", "", colnames(mat1)),
                      primary=colnames(mat1),
-                     colname=colnames(mat1), 
+                     colname=colnames(mat1),
                      condition=gsub("_\\w+", "", colnames(mat1)))
     return(map)
 }
@@ -85,7 +85,7 @@
     ll <- ess_list$experiments
     cdidx <- grep("coldata", names(ll))
     cd <- NULL
-    if (length(cdidx)!=0) 
+    if (length(cdidx)!=0)
     {
         cd <- ll[[cdidx]]
         ll <- ess_list$experiments[-cdidx]
@@ -94,7 +94,7 @@
     {
         x <- x[order(rownames(x)),]
     })
-    
+
     dims <- lapply(ll, dim)
     # expslist <- vector("list", length(ll))
     # sampmap <- DataFrame()
@@ -119,8 +119,8 @@
     } else {
         coldat <- cd
     }
-    mae <- MultiAssayExperiment::MultiAssayExperiment(experiments=expslist, 
-                                                      sampleMap=sampmap, 
+    mae <- MultiAssayExperiment::MultiAssayExperiment(experiments=expslist,
+                                                      sampleMap=sampmap,
                                                       colData=coldat)
     if(!isEmpty(grep("TCR", names(ll))))
     {
@@ -140,7 +140,7 @@
 #'
 #'     Available datasets are:
 #'     \itemize{
-#'         \item{cord_blood: } a dataset of single cells of cord blood as
+#'         \item{cord_blood:} a dataset of single cells of cord blood as
 #'         provided in Stoeckius et al. (2017).
 #'          \itemize{
 #'             \item{scRNA_Counts} - Stoeckius scRNA-seq gene count matrix
@@ -148,19 +148,19 @@
 #'             }
 #'      }
 #'      \itemize{
-#'         \item{peripheral_blood: } a dataset of single cells of peripheral 
+#'         \item{peripheral_blood: } a dataset of single cells of peripheral
 #'         blood as provided in Mimitou et al. (2019).
-#'         We provide two different conditions controls (CTRL) and 
+#'         We provide two different conditions controls (CTRL) and
 #'         Cutaneous T-cell Limphoma (CTCL).
-#'         Just build appropriate \code{modes} regex for subselecting the 
-#'         dataset modes. 
+#'         Just build appropriate \code{modes} regex for subselecting the
+#'         dataset modes.
 #'          \itemize{
 #'             \item{scRNA} - Mimitou scRNA-seq gene count matrix
 #'             \item{scADT} - Mimitou antibody-derived tags (ADT) data
 #'             \item{scHTO} - Mimitou Hashtag Oligo (HTO) data
-#'             \item{TCRab} - Mimitou T-cell Receptors (TCR) alpha and beta  
+#'             \item{TCRab} - Mimitou T-cell Receptors (TCR) alpha and beta
 #'             available through the object metadata.
-#'             \item{TCRgd} - Mimitou T-cell Receptors (TCR) gamma and delta  
+#'             \item{TCRgd} - Mimitou T-cell Receptors (TCR) gamma and delta
 #'             available through the object metadata.
 #'             }
 #'      }
@@ -170,12 +170,12 @@
 #'
 #' @param modes character() The assay types or modes of data to obtain these
 #'     include scADT and scRNA-seq data by default.
-#'     
+#'
 #' @param version character(1) Either version '1.0.0' depending on
 #'     data version required.
 #' @param dry.run logical(1) Whether to return the dataset names before actual
 #'     download (default TRUE)
-#' @param filtered logical(1) indicating if the returned dataset needs to 
+#' @param filtered logical(1) indicating if the returned dataset needs to
 #'     have filtered cells.
 #'     See Details for additional information about the filtering process.
 #'
@@ -184,35 +184,35 @@
 #'
 #' @param ... Additional arguments passed on to the
 #'     \link[ExperimentHub]{ExperimentHub-class} constructor
-#'     
+#'
 #' @param DataClass either MultiAssayExperiment or SingleCellExperiment
 #' data classes can be returned (default MultiAssayExperiment)
 #'
-#' @details 
+#' @details
 #' If `filtered` parameter is `FALSE` (default), the `colData` of the returned
-#' object contains multiple columns of `logicals` indicating the cells to be 
+#' object contains multiple columns of `logicals` indicating the cells to be
 #' discarded.
-#' In case `filtered` is `TRUE`, the `discard` column is used to filer the 
+#' In case `filtered` is `TRUE`, the `discard` column is used to filer the
 #' cells.
 #' Column `adt.discard` indicates the cells to be discarded computed on the ADT
 #' assay.
-#' Column `mito.discard` indicates the cells to be discarded computed on the 
-#' RNA assay and mitocondrial genes. 
+#' Column `mito.discard` indicates the cells to be discarded computed on the
+#' RNA assay and mitocondrial genes.
 #' Column `discard` combines the previous columns with an `OR` operator.
-#' Note that for the `peripheral_blood` dataset these three columns are 
+#' Note that for the `peripheral_blood` dataset these three columns are
 #' computed and returned separately for the `CTCL` and `CTRL` conditions.
-#' In this case the additional `discard` column combines the `discard.CTCL` and 
+#' In this case the additional `discard` column combines the `discard.CTCL` and
 #' `discard.CTRL` columns with an `OR` operator.
 #' Cell filtering has been computed for `cord_blood` and `peripheral_blood`
-#' datasets following section 12.3 of the Advanced Single-Cell Analysis with 
+#' datasets following section 12.3 of the Advanced Single-Cell Analysis with
 #' Bioconductor book.
-#' Executed code can be retrieved in the CITEseq_filtering.R script of this 
+#' Executed code can be retrieved in the CITEseq_filtering.R script of this
 #' package.
-#' 
+#'
 #' @return A single cell multi-modal \linkS4class{MultiAssayExperiment} or
 #'     informative `data.frame` when `dry.run` is `TRUE`.
 #'     When `DataClass` is `SingleCellExperiment` an object of this class
-#'     is returned with an RNA assay as main experiment and other assay(s) 
+#'     is returned with an RNA assay as main experiment and other assay(s)
 #'     as `AltExp(s)`.
 #' @references Stoeckius et al. (2017), Mimitou et al. (2019)
 #' @export
@@ -222,7 +222,7 @@
 #' mae <- CITEseq(DataType="cord_blood", dry.run=FALSE)
 #' experiments(mae)
 CITEseq <- function(DataType=c("cord_blood", "peripheral_blood"), modes="*",
-                version="1.0.0", dry.run=TRUE, filtered=FALSE, verbose=TRUE, 
+                version="1.0.0", dry.run=TRUE, filtered=FALSE, verbose=TRUE,
                 DataClass=c("MultiAssayExperiment", "SingleCellExperiment"),
                 ...)
 {
@@ -230,7 +230,7 @@ CITEseq <- function(DataType=c("cord_blood", "peripheral_blood"), modes="*",
     message("Dataset: ", dataType)
     dataClass <- match.arg(DataClass)
     ess_list <- .getResourcesList(prefix = "citeseq_", datatype = dataType,
-                    modes=modes, version=version, 
+                    modes=modes, version=version,
                     dry.run=dry.run, verbose=verbose, ...)
     if (!dry.run) {
         mae <- switch(
@@ -255,20 +255,20 @@ CITEseq <- function(DataType=c("cord_blood", "peripheral_blood"), modes="*",
 #' @description converts a `MultiAssayExperiment` object with CITEseq data into
 #' a `SingleCellExperiment` object to be used with already known methods and
 #' packages in literature.
-#' 
+#'
 #' Note that for creating a `SingleCellExperiment` object the following function
-#' subsets all the assays present in the `MultiAssayExperiment` with only the 
+#' subsets all the assays present in the `MultiAssayExperiment` with only the
 #' common cells across all the modalities.
 #' This could result in a not complete object.
 #'
 #'
-#' @param mae a MultiAssayExperiment object with scRNA and/or scADT and/or 
+#' @param mae a MultiAssayExperiment object with scRNA and/or scADT and/or
 #' scHTO named experiments.
 #'
 #' @return a SingleCellExperiment object as widely with scRNA data as counts
 #' and scADT, scHTO data as altExps.
 #' If only one modality is present, it has returned as main assay of the SCE.
-#' 
+#'
 #' @importFrom MultiAssayExperiment experiments
 #' @importFrom SummarizedExperiment SummarizedExperiment
 #' @importFrom SingleCellExperiment SingleCellExperiment altExps
@@ -277,27 +277,27 @@ CITEseq <- function(DataType=c("cord_blood", "peripheral_blood"), modes="*",
 .CITEseqMaeToSce <- function(mae)
 {
     stopifnot(c(is(mae, "MultiAssayExperiment"), !(length(mae)==0)))
-    
+
     if(length(mae)==3)
     {
         scrna <- experiments(mae)[[grep("scRNA", names(mae))]]
         scadt <- SingleCellExperiment(experiments(mae)[[grep("scADT", names(mae))]])
         schto <- SingleCellExperiment(experiments(mae)[[grep("scHTO", names(mae))]])
-        
+
         commonsamp <- intersect(intersect(colnames(scrna), colnames(scadt)), colnames(schto))
-        
+
         schto <- schto[,(colnames(schto) %in% commonsamp)]
         scrna <- scrna[,(colnames(scrna) %in% commonsamp)]
         scadt <- scadt[,(colnames(scadt) %in% commonsamp)]
-        
+
         sce <- SingleCellExperiment::SingleCellExperiment(
-            list(counts=scrna), 
+            list(counts=scrna),
             altExps=list(scADT=scadt, scHTO=schto)
         )
         cd <- colData(mae)
         idxcd <- which(commonsamp %in% rownames(cd))
         cdcs <- cd[idxcd,]
-        
+
         colData(sce) <- cdcs
     } else if(length(mae)==2) {
         scrna <- experiments(mae)[[grep("scRNA", names(mae))]]
@@ -310,7 +310,7 @@ CITEseq <- function(DataType=c("cord_blood", "peripheral_blood"), modes="*",
             name <- "scHTO"
         }
         commonsamp <- intersect(colnames(scrna), colnames(scalt))
-        
+
         scalt <- scalt[,(colnames(scalt) %in% commonsamp)]
         scrna <- scrna[,(colnames(scrna) %in% commonsamp)]
         l <- list(scalt)
@@ -334,7 +334,7 @@ CITEseq <- function(DataType=c("cord_blood", "peripheral_blood"), modes="*",
         idxcd <- unlist(which(colnames(sce) %in% rownames(cd)))
         colData(sce) <- cd[idxcd,]
     }
-    
+
     return(sce)
 }
 
